27d8ff05c5940c8d6a1f582959eb46ea393a3cf5
Lijo <lijo.belardi@icloud.com>
2022-05-31 00:22:14 +0200

    created carousel

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+const Card = (props) => {
+  return (
+    <div key={props.key} className={styles.card}>
+        {/* TODO aggiungere link */}
+        <p>{props.title}</p>
+        <img src={props.img} alt={props.title} />
+        <div className={styles.gradient}></div>
+    </div>
+  )
+}
+
+export default Card
--- src/components/PopularVegetarianCarousel/index.jsx
+++ src/components/PopularVegetarianCarousel/index.jsx
@@ -1,13 +1,36 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import { PopularVegetarianContext } from '../../context/popularVegetariancontext'
+import Card from '../Card';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
+import styles from './index.module.scss'
 
 const PopularVegetarianCarousel = () => {
   const [recipes, setRecipes] = useContext(PopularVegetarianContext)
   return (
     <div>
-      {recipes.map((recipe) => {
-        return <p key={recipe.id}>{recipe.title}</p>
-      })}
+      <h3>Vegetarian Picks</h3>
+      <Splide
+        className={styles.splide}
+        options={{
+          perPage: 3,
+          arrows: true,
+          pagination: false,
+          drag: 'free',
+          gap: '5rem',
+        }}>
+
+        {recipes.map((recipe) => {
+          return <SplideSlide key={recipe.id}>
+            <Card
+              key={recipe.id}
+              id={recipe.id}
+              title={recipe.title}
+              img={recipe.image} />
+          </SplideSlide>
+        })}
+      </Splide>
+
     </div>
   )
 }
