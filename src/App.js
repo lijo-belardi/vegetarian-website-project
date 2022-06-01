ba86305e9622310ea18698dff52013f2bbb216ac
Lijo <lijo.belardi@icloud.com>
2022-06-01 18:14:47 +0200

    fix(Routes): Fixed Routes's error.
    Error's body: "router.ts:5 Uncaught Error: useRoutes() may be used only in the context of a <Router> component."
    
    Added "BrowserRouter as Router" and wrapped the App.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import { PopularVegetarianProvider } from "./context/popularVegetariancontext";
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Recipe from "./pages/Recipe";
 
 function App() {
   return (
     <PopularVegetarianProvider>
-      <div className="App">
-        <Home />
-      </div>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/recipe/:name" element={<Recipe />} />
+          </Routes>
+
+        </div>
+      </Router>
     </PopularVegetarianProvider>
   );
 }
