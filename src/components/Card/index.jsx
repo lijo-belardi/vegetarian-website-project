6b287d2fb0879cb844f21e364528dc2b582d135c
Lijo <lijo.belardi@icloud.com>
2022-06-06 18:19:42 +0200

    Deleted TODO

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,7 +4,6 @@ import styles from './index.module.scss'
 const Card = (props) => {
   return (
     <div className={styles.card}>
-        {/* TODO aggiungere link */}
         <p>{props.title}</p>
         <img src={props.img} alt={props.title} />
         <div className={styles.gradient}></div>
