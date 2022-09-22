fdf091067929e1616d21f32434de7c54eeb60e3c
Lijo <lijo.belardi@icloud.com>
2022-09-22 17:10:17 +0200

    Improved footer's section with links to social networks.

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,51 @@
-import { Container, Typography } from '@mui/material'
 import React from 'react'
+import { Container, Typography, Grid, Box } from '@mui/material'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
 import styles from './index.module.scss'
 
 const Footer = () => {
   return (
     <div className={styles['footer']}>
-      <Container>
-        <Typography variant='h6' align='center'>© 2022 Copyright: Lijo Belardi</Typography>
+      <Container sx={{ minHeight: ['8rem'] }}>
+
+        {/* Grid - container */}
+        <Grid container>
+          <Grid item xs={12}>
+            <Box sx={{
+              display: 'flex',
+              flexDirection: ['row', 'row'],
+              alignItems: 'center',
+              justifyContent: 'space-around',
+              minHeight: ['4rem', '4rem'],
+              borderBottom: '1px solid #000'
+            }}>
+              <GitHubIcon
+                onClick={() => window.open('https://github.com/lijo-belardi', '_blank')}
+                sx={{ '&:hover': { cursor: 'pointer', backgroundColor: '#99db9b', color: '#38ad60' } }}
+              />
+              <LinkedInIcon
+                onClick={() => window.open('https://www.linkedin.com/in/lijo-belardi/', '_blank')}
+                sx={{ '&:hover': { cursor: 'pointer', backgroundColor: '#99db9b', color: '#38ad60' } }}
+              />
+              <InstagramIcon
+                onClick={() => window.open('https://www.instagram.com/lijo.belardi/', '_blank')}
+                sx={{ '&:hover': { cursor: 'pointer', backgroundColor: '#99db9b', color: '#38ad60' } }}
+              />
+              <FacebookIcon
+                onClick={() => window.open('https://www.facebook.com/lijo.belardi.75', '_blank')}
+                sx={{ '&:hover': { cursor: 'pointer', backgroundColor: '#99db9b', color: '#38ad60' } }}
+              />
+            </Box>
+          </Grid>
+        </Grid>
+
+        {/* Copyright */}
+        <Typography variant='h6' align='center' marginTop={3}>
+          © 2022 Copyright: Lijo Belardi
+        </Typography>
       </Container>
     </div>
   )
